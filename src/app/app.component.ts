c423e8e1673af23db62e588223f826d946578065
Renan Souza <renansouza0122@gmail.com>
2024-01-23 20:07:34 -0300

    start component and start animation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger);
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+        const amountScroll = (document.querySelector('h1') as HTMLElement)?.offsetWidth;
+
+        gsap.to("h1", {
+          xPercent: () => (amountScroll - window.innerWidth),
+          ease: "none",
+          scrollTrigger: {
+            trigger: ".start",
+            pin: true,
+            scrub: 1,
+            end: "+=600%", 
+      
+          },
+        });
+  }
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from 'src/app/components/header/header.component';
+import { StartComponent } from 'src/app/components/start/start.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,StartComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
