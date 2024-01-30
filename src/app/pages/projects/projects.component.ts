ec5ae0e4ac253f2800700f44e1a5ff761943e1dd
RenanSouza <renansouza0122@gmail.com>
2024-01-29 23:36:10 -0300

    adding title

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from 'src/app/components/title/title.component';
 import { CursorComponent } from 'src/app/ui/cursor/cursor.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CursorComponent],
+  imports: [CursorComponent,TitleComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
