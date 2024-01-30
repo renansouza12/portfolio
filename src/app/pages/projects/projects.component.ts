562bb5fa4fc904d2d1e0e72c187a7545f64fdf12
Renan Souza <renansouza0122@gmail.com>
2024-01-30 18:30:29 -0300

    card animations added

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleComponent } from 'src/app/components/title/title.component';
 import { CursorComponent } from 'src/app/ui/cursor/cursor.component';
+import { ProjectsCatalogComponent } from 'src/app/components/projects-catalog/projects-catalog.component';
 
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ProjectsCatalogComponent } from 'src/app/components/projects-catalog/projects-catalog.component';
+
 
 gsap.registerPlugin(ScrollTrigger);
 @Component({
@@ -26,11 +27,10 @@ export class ProjectsComponent implements OnInit{
     gsap.to('.title',{
       opacity:0,
       stagger:0.2,
-      duration:2  ,
       scrollTrigger:{
         trigger:'.title_container',
-        start:'0% top',
-        end:'bottom center',
+        start:'top top',
+        end:'center center',
         scrub:1,
       }
     })
--- src/app/ui/project-card/project-card.component.ts
+++ src/app/ui/project-card/project-card.component.ts
@@ -1,5 +1,9 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-project-card',
   standalone: true,
@@ -7,7 +11,20 @@ import { Component,Input } from '@angular/core';
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit{
   @Input() id!:number;
   @Input() imageCoverCard!:string;
+
+  ngOnInit(): void {
+    //gsap animation
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.projects_catalog',
+        start: "-40% top",
+        end: "center center",
+        scrub: true,
+        markers:true
+      }
+    }).fromTo('.project_card', { transition:1,opacity: 0 ,yPercent:300}, { opacity: 1, stagger: 0.4 ,yPercent:0})
+  }
 }
