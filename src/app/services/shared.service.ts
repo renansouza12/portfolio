5f5179079d5ff127a8f2824e46dbf76a2be58f2b
RenanSouza <renansouza0122@gmail.com>
2024-02-14 16:00:58 -0300

    alura card profile added on projects

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -13,9 +13,9 @@ export class SharedService {
       'name':"Keytap",
       'description':"A straightforward website showcasing keyboards and keyboard keys, allowing users to effortlessly add items to the cart and adjust quantities as needed.",
       images:{
-        firstImage:"../../../assets/images/keytap-image.png",
-        secondImage:"../../../assets/images/keytap-image1.png",
-        thirdImage:"../../../assets/images/keytap-image2.png"
+        firstImage:"../../../assets/images/keytap-images/keytap-image.png",
+        secondImage:"../../../assets/images/keytap-images/keytap-image1.png",
+        thirdImage:"../../../assets/images/keytap-images/keytap-image2.png"
       },
       website:'https://keytap.vercel.app/',
       repository:'https://github.com/renansouza12/keytap',
@@ -27,9 +27,9 @@ export class SharedService {
       'name':"Fuyu",
       'description':"A captivating landing page dedicated to the Y-3 shoe, skillfully crafted with Angular and GreenSock animations, ensuring a seamless and visually dynamic experience across all devices with responsive design.",
       images:{
-        firstImage:"../../../assets/images/fuyu-image.png",
-        secondImage:"../../../assets/images/fuyu-image1.avif",
-        thirdImage:"../../../assets/images/fuyu-image2.avif"
+        firstImage:"../../../assets/images/fuyu-images/fuyu-image.png",
+        secondImage:"../../../assets/images/fuyu-images/fuyu-image1.avif",
+        thirdImage:"../../../assets/images/fuyu-images/fuyu-image2.avif"
       },
       website:'https://fuyu-landing-page.vercel.app/',
       repository:'https://github.com/renansouza12/FUYU-landing-page',
@@ -41,9 +41,9 @@ export class SharedService {
       'name':"Card Details",
       'description':"An interactive card details form where you can effortlessly modify the cardholder's name, card number, birthdate, and CVC for a personalized and secure input experience.",
       images:{
-        firstImage:"../../../assets/images/cardDetails-image.png",
-        secondImage:"../../../assets/images/cardDetails-image1.avif",
-        thirdImage:"../../../assets/images/cardDetails-image2.avif"
+        firstImage:"../../../assets/images/card-details-images/cardDetails-image.png",
+        secondImage:"../../../assets/images/card-details-images/cardDetails-image1.avif",
+        thirdImage:"../../../assets/images/card-details-images/cardDetails-image2.avif"
       },
       website:'https://gregarious-crumble-17bc55.netlify.app/',
       repository:'https://github.com/renansouza12/card-details-form',
@@ -55,9 +55,9 @@ export class SharedService {
       'name':"Quiz",
       'description':"A dynamic 'Discover Personality Type' quiz consisting of five questions, each generating four options dynamically to provide a personalized and engaging experience for users.",
       images:{
-        firstImage:"../../../assets/images/quiz-image.png",
-        secondImage:"../../../assets/images/quiz-image1.avif",
-        thirdImage:"../../../assets/images/quiz-image2.avif"
+        firstImage:"../../../assets/images/quiz-images/quiz-image.png",
+        secondImage:"../../../assets/images/quiz-images/quiz-image1.avif",
+        thirdImage:"../../../assets/images/quiz-images/quiz-image2.avif"
       },
       website:'https://quiz-pink-delta.vercel.app/',
       repository:'https://github.com/renansouza12/quiz',
@@ -69,13 +69,27 @@ export class SharedService {
       'name':"Decoder",
       'description':"An application for encrypting and decrypting texts, where the letter 'e' is converted to 'enter,' 'i' to 'imes,' 'a' to 'ai,' 'o' to 'ober,' and 'u' to 'ufat'.",
       images:{
-        firstImage:"../../../assets/images/decoder-image.png",
-        secondImage:"../../../assets/images/decoder-image1.avif",
-        thirdImage:"../../../assets/images/decoder-image2.avif"
+        firstImage:"../../../assets/images/decoder-images/decoder-image.png",
+        secondImage:"../../../assets/images/decoder-images/decoder-image1.avif",
+        thirdImage:"../../../assets/images/decoder-images/decoder-image2.avif"
       },
       website:'https://decoder-alura-desafio.vercel.app/',
       repository:'https://github.com/renansouza12/decoder-alura-desafio',
-      techs:'Html css javascript',
+      techs:'Html scss javascript',
+      level:'Newba'
+    },
+    {
+      'id':6,
+      'name':"Alura Card Profile",
+      'description':"A simple card using alura's API and displaying information about my profile on Alura's website, such as my ID, completed courses, and total steps.",
+      images:{
+        firstImage:"../../../assets/images/alura-card-images/alura-card.png",
+        secondImage:"../../../assets/images/alura-card-images/alura-card1.png",
+        thirdImage:"../../../assets/images/alura-card-images/alura-card2.png"
+      },
+      website:'https://alura-card-profile.netlify.app/',
+      repository:'https://github.com/renansouza12/alura-card-profile/tree/main?tab=readme-ov-file',
+      techs:'Html scss javascript',
       level:'Newba'
     },
   ]
