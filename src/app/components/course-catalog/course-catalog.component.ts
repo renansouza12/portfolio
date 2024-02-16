a43ecd743eb648fa4280599bd65da0e20976f67a
Renan Souza <renansouza0122@gmail.com>
2024-02-15 23:14:06 -0300

    adding hover image

--- src/app/components/course-catalog/course-catalog.component.ts
+++ src/app/components/course-catalog/course-catalog.component.ts
@@ -9,7 +9,8 @@ import { Certificates } from 'src/app/models/projects.model';
   templateUrl: './course-catalog.component.html',
   styleUrl: './course-catalog.component.scss'
 })
-export class CourseCatalogComponent {
+export class CourseCatalogComponent{
+  
   certificates:Certificates[]=[
     {
       name:"index"
@@ -57,8 +58,32 @@ export class CourseCatalogComponent {
       name:"index"
     },
   ]
-  boxCard():void{
-    console.log('clicked');
+
+
+  private updateCursorOutline(action: 'add' | 'remove', i: number): void {
+    const firstImage = document.querySelector(".first_image");
+    const secondImage = document.querySelector(".second_image");
+
+    if (i === 4) {
+      firstImage?.classList[action]('active');
+      secondImage?.classList.remove('active');
+    } else if (i === 7) {
+      secondImage?.classList[action]('active');
+      firstImage?.classList.remove('active');
+    }
+  }
+
+  mouseEnter(i: number): void {
+    if (i === 4 || i === 7) {
+      this.updateCursorOutline('add', i);
+    }
+  }
+
+  mouseLeave(i: number): void {
+    console.log('leaved');
     
+    if (i === 4 || i === 7) {
+      this.updateCursorOutline('remove', i);
+    }
   }
 }
